ee1eed11824dcb30a30a70ed8364bcdfce944c0c
nickheal <nick3_4@hotmail.com>
2020-09-12 18:45:29 +0100

    feat: added TS definitions

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import babel from 'rollup-plugin-babel';
+import typescript from 'rollup-plugin-typescript2';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   output: [
     {
       file: 'cjs/index.js',
@@ -12,5 +12,5 @@ export default {
       format: 'es',
     },
   ],
-  plugins: [babel()],
+  plugins: [typescript()],
 };
